e9408dfdd3fcb3b0449508c6571ff1146cb1dc59
OniasC <oniascbs@gmail.com>
2018-06-17 13:47:26 -0400

    modifica canal para 43

--- src/control/Robo.cpp
+++ src/control/Robo.cpp
@@ -37,7 +37,7 @@ Robo::Robo(Motor *roboMotor0, Motor *roboMotor1, Motor *roboMotor2, Motor *roboM
 
 	_id = Switch->id;
 
-	channel=92;
+	channel=43;
 	address=0xE7E7E7E700;
 	last_packet_ms = 0;
 }
